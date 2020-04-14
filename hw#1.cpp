d40a735e3e390a39e7fea5b637974cedb67827d3
Марк Парфенов <parfenoff@router.lab209.kaf>
2020-04-14 12:39:04 +0300

    Create easy calc

---
+++ hw#1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+setlocale(LC_ALL,"RU");
+
+float a,b,c;
+
+cout << "Введите значения переменных a,b,c" << endl;
+
+cin >> a >> b >> c;
+
+cout << "Сумма переменных равна:" << a + b + c << endl;
+
+cout << "Произведение переменных равно:" << a * b * c << endl;
+
+cout << "Среднее арифметическое переменных:" << (a + b+ c) / 3 << endl; 
+
+}
